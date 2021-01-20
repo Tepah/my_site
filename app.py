04934cf190e879193c1a93e07765958e986076f5
Pete Potipitak <TepahT@gmail.com>
2021-01-20 13:51:07 -0800

    Page starting
    Started making the pages to create a page with links to projects and pages to showcase the projects once clicked.

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ import flask
 app = flask.Flask(__name__)
 
 
+def get_projects():
+    return [
+        {'name': 'Space Pew', 'type': 'Game'},
+        {'name': 'Stat tracker', 'type': 'Data logger'},
+        {'name': 'Pete-bot', 'type': 'Discord Bot'}
+    ]
+
+
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    test_projects = get_projects()
+    return flask.render_template('index.html', projects=test_projects)
 
 
 if __name__ == '__main__':
