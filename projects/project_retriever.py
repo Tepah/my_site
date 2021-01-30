d57494a50f9afecbc7a54119ccf13ee9601f28e5
Pete Potipitak <TepahT@gmail.com>
2021-01-30 14:34:29 -0800

    started about page

--- projects/project_retriever.py
+++ projects/project_retriever.py
@@ -1,6 +1,7 @@
 def get_projects():
     return [
-        {'name': 'Space Pew', 'type': 'Game', 'directory': 'space_pew'},
-        {'name': 'Stat Tracker', 'type': 'Data logger', 'directory': 'stat_tracker'},
-        {'name': 'Pete-bot', 'type': 'Discord Bot', 'directory': 'pete-bot'}
+        {'name': 'Space Pew', 'description': 'A game made with pygame!', 'type': 'Game', 'directory': 'space_pew'},
+        {'name': 'Stat Tracker', 'description': 'A stat logger', 'type': 'Data logger', 'directory': 'stat_tracker'},
+        {'name': 'Pete-bot[WIP]', 'description': 'A work in progress discord bot', 'type': 'Discord Bot',
+         'directory': 'pete-bot'}
     ]
