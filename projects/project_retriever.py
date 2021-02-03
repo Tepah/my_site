174ca69f9f8031a9c2a814ee276ce770f3be2657
Pete Potipitak <TepahT@gmail.com>
2021-02-03 13:45:15 -0800

    Automatically gets pictures and descriptions from file.

--- projects/project_retriever.py
+++ projects/project_retriever.py
@@ -1,9 +1,16 @@
 def get_projects():
     return {
         'space_pew': {'name': 'Space Pew', 'description': 'A game made with pygame!', 'type': 'Game',
-                      'directory': 'space_pew'},
+                      'directory': 'space_pew', 'description_1': "Gamers love this one because it's easy",
+                      'description_2': "I play fortnite when I'm bored",
+                      'description_3': "I'm kind of hungry right now."},
         'stat_tracker': {'name': 'Stat Tracker', 'description': 'A stat logger', 'type': 'Data logger',
-                         'directory': 'stat_tracker'},
-        'pete-bot': {'name': 'Pete-bot[WIP]', 'description': 'A work in progress discord bot', 'type': 'Discord Bot',
-                     'directory': 'pete-bot'},
+                         'directory': 'stat_tracker', 'description_1': "Wowso",
+                         'description_2': "Spoons aren't even",
+                         'description_3': "Hurry, my bowls exploded"},
+        'pete-bot': {'name': 'Pete-bot[WIP]', 'description': 'A work in progress discord bot',
+                     'type': 'Discord Bot',
+                     'directory': 'pete-bot', 'description_1': "I'm getting itchy",
+                     'description_2': "I have a tuna salad sandwich ready",
+                     'description_3': "Does anybody have any pickles?"},
     }
